01cb0cf6c84b129bc1e93ebf80ce2306d5ab2e46
Denver Schouw <dschouw@gmail.com>
2022-11-19 13:51:01 +0200

    feat(server) Baseline checkin of server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+// require the JSON files in the data folder
+const Harrow = require('./data/Harrow.json');
+const Heathrow = require('./data/Heathrow.json');
+const Statford = require('./data/Stratford.json');
+
+app.get('/', (request,response) => {
+    response.send(
+        {
+            SupportedRoutes : [
+                '/Pharmacies',
+                '/Doctors',
+                '/Colleges',
+                '/Hospitals',
+            ],
+            Version:"0.0.0.1",
+            Author: "dschouw"
+        });
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
